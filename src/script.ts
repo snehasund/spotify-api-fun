d4297c301c9724c4bcda7a4c82706f61467f8242
Sneha Sundar <snehas9@illinois.edu>
2024-05-23 23:21:49 -0700

    top tracks - undefined?

--- src/script.ts
+++ src/script.ts
@@ -1,5 +1,4 @@
 import { redirectToAuthCodeFlow, getAccessToken } from './authCodeWithPkce';
-
 const clientId = "efe05af2049e4cdfa23e2a047520440d";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
@@ -10,8 +9,8 @@ const code = params.get("code");
     } else {
         const accessToken = await getAccessToken(clientId, code);
         const profile = await fetchProfile(accessToken);
-        const topTracks = await getTopTracks(accessToken, "long_term", 20, 0);
-        const topArtists = await getTopArtists(accessToken, "long_term", 20, 0);
+        const topTracks = await getTopTracks(accessToken);
+        const topArtists = await getTopArtists(accessToken);
         populateUI(profile, topTracks, topArtists);
     }
 })();
@@ -24,16 +23,16 @@ async function fetchProfile(accessToken: string): Promise<UserProfile> {
     return await result.json();
 }
 
-async function getTopTracks(accessToken: string, time_range: string, limit: number, offset: number) {
-    const topTracks = await fetch(`https://api.spotify.com/v1/me/top/tracks?limit=${limit}&time_range=${time_range}&offset=${offset}`, {
+async function getTopTracks(accessToken: string) {
+    const topTracks = await fetch('https://api.spotify.com/v1/me/top/tracks', {
         method: 'GET', headers: { Authorization: `Bearer ${accessToken}` }
     });
 
     return await topTracks.json();
 }
 
-async function getTopArtists(accessToken: string, time_range: string, limit: number, offset: number) {
-    const topArtists = await fetch(`https://api.spotify.com/v1/me/top/artists?limit=${limit}&time_range=${time_range}&offset=${offset}`, {
+async function getTopArtists(accessToken: string) {
+    const topArtists = await fetch(`https://api.spotify.com/v1/me/top/artists`, {
         method: 'GET', headers: { Authorization: `Bearer ${accessToken}` }
     });
 
@@ -50,20 +49,6 @@ function populateUI(profile: UserProfile, topTracks: any, topArtists: any) {
     document.getElementById("url")!.innerText = profile.href;
     document.getElementById("url")!.setAttribute("href", profile.href);
     document.getElementById("imgUrl")!.innerText = profile.images[0].url;
-
-    //display top tracks
-    const topTracksList = document.getElementById("topTracks")!;
-    topTracks.items.forEach((track: any) => {
-        const trackItem = document.createElement("li");
-        trackItem.innerText = track.name;
-        topTracksList.appendChild(trackItem);
-    });
+    document.getElementById("topTrack")!.innerText = topTracks[1];
     
-    //display top artists
-    const topArtistsList = document.getElementById("topArtists")!;
-    topArtists.items.forEach((artist: any) => {
-        const artistItem = document.createElement("li");
-        artistItem.innerText = artist.name;
-        topArtistsList.appendChild(artistItem);
-    });
 }
