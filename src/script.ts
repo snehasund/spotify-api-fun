c2fceacdd808d8361a05e7919dc698e68c81d2e7
Sneha Sundar <snehasundar@Arrcuss-MacBook-Pro-3.local>
2024-06-06 12:02:07 -0700

    spotify api hates me

--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,5 @@
 import { redirectToAuthCodeFlow, getAccessToken } from './authCodeWithPkce';
+
 const clientId = "efe05af2049e4cdfa23e2a047520440d";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
@@ -9,9 +10,9 @@ const code = params.get("code");
     } else {
         const accessToken = await getAccessToken(clientId, code);
         const profile = await fetchProfile(accessToken);
-        const topTracks = await getTopTracks(accessToken);
+        const topsons = await getTopTracks(accessToken);
         const topArtists = await getTopArtists(accessToken);
-        populateUI(profile, topTracks, topArtists);
+        populateUI(accessToken, profile, topsons.items);
     }
 })();
 
@@ -23,12 +24,14 @@ async function fetchProfile(accessToken: string): Promise<UserProfile> {
     return await result.json();
 }
 
-async function getTopTracks(accessToken: string) {
-    const topTracks = await fetch('https://api.spotify.com/v1/me/top/tracks', {
-        method: 'GET', headers: { Authorization: `Bearer ${accessToken}` }
+async function getTopTracks(token) {
+    var tokstr = "Bearer " + token;
+    var term = "";
+    term = "long_term";
+    const result = await fetch("https://api.spotify.com/v1/me/top/tracks?limit=100&time_range=" + term, {
+        method: "GET", headers: { Authorization: tokstr }
     });
-
-    return await topTracks.json();
+    return await result.json();
 }
 
 async function getTopArtists(accessToken: string) {
@@ -39,7 +42,7 @@ async function getTopArtists(accessToken: string) {
     return await topArtists.json();
 }
 
-function populateUI(profile: UserProfile, topTracks: any, topArtists: any) {
+function populateUI(profile: UserProfile, topsongs, topArtists: any) {
     document.getElementById("displayName")!.innerText = profile.display_name; 
     document.getElementById("avatar")!.setAttribute("src", profile.images[0].url);
     document.getElementById("id")!.innerText = profile.id;
@@ -49,6 +52,17 @@ function populateUI(profile: UserProfile, topTracks: any, topArtists: any) {
     document.getElementById("url")!.innerText = profile.href;
     document.getElementById("url")!.setAttribute("href", profile.href);
     document.getElementById("imgUrl")!.innerText = profile.images[0].url;
-    document.getElementById("topTrack")!.innerText = topTracks[1];
     
+    // how do i display top artists & top tracks
+    var song1 = topsongs[0]
+    var song2 = topsongs[1]
+    var song3 = topsongs[2]
+
+
+    document.getElementById("songs").innerHTML = "<strong>Your top songs:</strong> <br>"
+    for (let i = 0; i < topsongs.length; i++) {
+        var num = i + 1;
+        var artists = topsongs[i].artists;
+        document.getElementById("songs").innerHTML += num + ". " + topsongs[i].name + "<br>";
+    }
 }
